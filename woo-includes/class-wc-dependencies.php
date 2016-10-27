c2861fccb5d4d2958f8800652e72f809ce41e365
Panos Kountanis <panosktn@gmail.com>
2016-10-27 19:39:21 +0300

    WC_Dependencies methods should be static
    Otherwise we get Strict Standards warnings
    
    closes #24

--- woo-includes/class-wc-dependencies.php
+++ woo-includes/class-wc-dependencies.php
@@ -8,7 +8,7 @@ class WC_Dependencies {
 
 	private static $active_plugins;
 
-	function init() {
+	public static function init() {
 
 		self::$active_plugins = (array) get_option( 'active_plugins', array() );
 
@@ -16,14 +16,11 @@ class WC_Dependencies {
 			self::$active_plugins = array_merge( self::$active_plugins, get_site_option( 'active_sitewide_plugins', array() ) );
 	}
 
-	function woocommerce_active_check() {
+	public static function woocommerce_active_check() {
 
 		if ( ! self::$active_plugins ) self::init();
 
 		return in_array( 'woocommerce/woocommerce.php', self::$active_plugins ) || array_key_exists( 'woocommerce/woocommerce.php', self::$active_plugins );
-
 	}
 
 }
-
-
