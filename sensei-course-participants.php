cf414aed41b2c5c5fe45a847758c41cfb570a97e
Jake Oehler Morrison <jake.morrison@automattic.com>
2019-03-12 11:45:52 +0000

    Remove woo updater functions and use `Woo` header

--- sensei-course-participants.php
+++ sensei-course-participants.php
@@ -10,6 +10,7 @@
  * Tested up to: 4.1
  * Text Domain: sensei-course-participants
  * Domain Path: /languages/
+ * Woo: 435834:f6479a8a3a01ac11794f32be22b0682f
  *
  * @package WordPress
  * @author Automattic
@@ -21,18 +22,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Required functions
+ * Functions used by plugins
  */
-if ( ! function_exists( 'woothemes_queue_update' ) ) {
-	require_once( __DIR__ . '/woo-includes/woo-functions.php' );
+if ( ! class_exists( 'WooThemes_Sensei_Dependencies' ) ) {
+	require_once( __DIR__ . '/woo-includes/class-woothemes-sensei-dependencies.php' );
 }
-
-/**
- * Plugin updates
- */
-woothemes_queue_update( plugin_basename( __FILE__ ), 'f6479a8a3a01ac11794f32be22b0682f', 435834 );
-
-require_once dirname( __FILE__ ) . '/includes/class-sensei-course-participants-dependency-checker.php';
 
 if ( ! Sensei_Course_Participants_Dependency_Checker::are_dependencies_met() ) {
 	return;
